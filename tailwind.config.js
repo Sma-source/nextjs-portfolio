40711da4572cca0288d175c39be86b4054d02dbb
Sma-source <rabhi.smain@gmail.com>
2023-05-30 23:02:43 +0200

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,10 @@ module.exports = {
         instagram: "#E4405F",
         twitter: "#1DA1F2",
         linkedin: "#0A66C2",
+        "neon-pink": "#F637EC",
+      },
+      boxShadow: {
+        "neon-pink": "0px 0px 15px #F637EC",
       },
     },
   },
