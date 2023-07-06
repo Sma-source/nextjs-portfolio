15d79be1e9fa5e2eab8d790891321075bc16b887
Sma-source <rabhi.smain@gmail.com>
2023-07-06 22:34:46 +0200

    change text

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between items-center py-4 px-2">
       <div className="flex items-center gap-[1ch]">
-        <div className="w-5 h-5 bg-yellow-400 rounded-full" />
+        <div className="w-5 h-5 bg-purple-900/60 rounded-full" />
         <span className="text-sm font-bold text-white tracking-widest uppercase">
           smadev
         </span>
