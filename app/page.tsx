8f361b24dd23458347dbfa2d42adac14eed3c30b
Sma-source <rabhi.smain@gmail.com>
2024-06-24 13:54:59 +0200

    import grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
+import Grid from "@/components/Grid";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Header />
+        <Grid />
       </div>
     </main>
   );
