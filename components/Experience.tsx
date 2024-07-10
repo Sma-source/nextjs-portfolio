f264022227f777b13210739cc82f5beb78335363
Sma-source <rabhi.smain@gmail.com>
2024-07-10 17:35:54 +0200

    create experients component section

---
+++ components/Experience.tsx
@@ -0,0 +1,49 @@
+import { workExperience } from "@/data";
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div className="py-20 w-full">
+      <h1 className="heading text-neutral-200">
+        Mon expérience
+        <span className="text-purple"> professionnelle</span>
+      </h1>
+      <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
+        {workExperience.map((card) => (
+          <div
+            key={card.id}
+            style={{
+              //   add these two
+              //   you can generate the color from here https://cssgradient.io/
+              background: "rgb(4,7,29)",
+              backgroundColor:
+                "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+              // add this border radius to make it more rounded so that the moving border is more realistic
+              borderRadius: `calc(1.75rem* 0.96)`,
+            }}
+            // remove bg-white dark:bg-slate-900
+            className="bg-transparent shadow-xl border border-white/[0.1]  shadow-white/[0.05] relative text-xl p-[1px] overflow-hidden md:col-span-2 md:row-span-1 flex-1 text-white border-slate-800"
+          >
+            <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
+              <img
+                src={card.thumbnail}
+                alt={card.thumbnail}
+                className="lg:w-32 md:w-20 w-16"
+              />
+              <div className="lg:ms-5">
+                <h1 className="text-start text-xl md:text-2xl font-bold">
+                  {card.title}
+                </h1>
+                <p className="text-start text-white-100 mt-3 font-semibold">
+                  {card.desc}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
