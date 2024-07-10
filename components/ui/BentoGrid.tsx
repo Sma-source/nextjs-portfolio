c296a4c633dcf0c6fb967ff24c4bcecc96dbb2e1
Sma-source <rabhi.smain@gmail.com>
2024-07-10 17:41:41 +0200

    modify bentogrid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,15 @@
 import { cn } from "@/utils/cn";
+import { useState } from "react";
+import {
+  FaReact,
+  FaNode,
+  FaPython,
+  FaWordpress,
+  FaVuejs,
+} from "react-icons/fa";
+import { IoCopyOutline, IoLogoJavascript } from "react-icons/io5";
+import { SiPhp, SiNextdotjs, SiGatsby, SiTypescript } from "react-icons/si";
+import MyButton from "../MyButton";
 
 export const BentoGrid = ({
   className,
@@ -10,7 +21,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
+        "grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-8 max-w-7xl w-full mx-auto",
         className
       )}
     >
@@ -28,8 +39,10 @@ export const BentoGridItem = ({
   imgClassName,
   titleClassName,
   spareImg,
+  spareImgClassName,
 }: {
   className?: string;
+
   id: number;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
@@ -37,7 +50,34 @@ export const BentoGridItem = ({
   imgClassName?: string;
   titleClassName?: string;
   spareImg?: string;
+  spareImgClassName?: string;
 }) => {
+  const leftLists = [
+    { name: "Typecript", icon: SiTypescript, color: "#3178C6" },
+    { name: "Javascript", icon: IoLogoJavascript, color: "#F7DF1E" },
+    { name: "Php", icon: SiPhp, color: "#777BB4" },
+  ];
+  const middleLists = [
+    { name: "React", icon: FaReact, color: "#61dbfb" },
+    { name: "Wordpress", icon: FaWordpress, color: "#21759B" },
+    { name: "Nodejs", icon: FaNode, color: "#539e43" },
+  ];
+  const rightLists = [
+    { name: "Vuejs", icon: FaVuejs, color: "#42b883" },
+    { name: "Nextjs", icon: SiNextdotjs, color: "#0B0B0B" },
+    { name: "Gatsbyjs", icon: SiGatsby, color: "#663399" },
+  ];
+  const [copied, setCopied] = useState(false);
+  const handleCopy = () => {
+    const text = "smain.rabhi@gmail.com";
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+
+    // Set a timeout to revert the copied state back to false after 3 seconds
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  };
   return (
     <div
       className={cn(
@@ -45,14 +85,13 @@ export const BentoGridItem = ({
         className
       )}
       style={{
-        //   add these two
-        //   you can generate the color from here https://cssgradient.io/
         background: "rgb(4,7,29)",
+        backgroundSize: "30%",
         backgroundColor:
-          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%) ",
       }}
     >
-      <div className={`${id === 6 && "flex justify-center"} h-full`}>
+      <div className={`${id === 6 && "flex justify-center"}  h-full`}>
         <div className="w-full h-full absolute">
           {img && (
             <img
@@ -72,17 +111,137 @@ export const BentoGridItem = ({
               src={spareImg}
               alt={spareImg}
               //   width={220}
-              className="object-cover object-center w-full h-full"
+              className={cn(
+                spareImgClassName,
+                "object-cover object-center w-full h-full "
+              )}
             />
           )}
         </div>
-      </div>
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
+        {id === 6 && (
+          // add background animation , remove the p tag
+          <div className="flex justify-center h-full">
+            <div className="w-full h-full absolute overflow-hidden top-0 left-0 gradient-background">
+              <div className="absolute  z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
+            </div>
+          </div>
+        )}
+
+        {/* <div className="group-hover/bento:translate-x-2 transition duration-200">
         <div className="font-sans font-bold text-neutral-200 mb-2 mt-2">
           {title}
         </div>
         <div className="font-sans font-normal  text-xs text-neutral-300">
           {description}
+        </div>
+      </div> */}
+        <div
+          className={cn(
+            titleClassName,
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+          )}
+        >
+          {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
+
+          <div
+            className={`
+              
+                font-sans text-balance text-base md:text-xl lg:text-2xl z-10  max-w-96 font-semibold  text-neutral-100
+                
+             `}
+          >
+            {title}
+          </div>
+          <div
+            className={` ${
+              id === 1
+                ? "font-sans pt-3 font-light md:max-w-lg tracking-wide md:text-base lg:text-base text-sm text-neutral-300 z-10"
+                : "font-sans pt-3 font-light md:max-w-96 tracking-wide md:text-xs lg:text-base text-sm text-neutral-300 z-10"
+            } 
+                 
+            } `}
+          >
+            {description}
+          </div>
+
+          {id === 2 && (
+            <div className="mt-5 relative mx-auto">
+              <img
+                className="rounded-full w-28 h-28"
+                src="/profile-pic 3.png"
+              />
+            </div>
+          )}
+          {/* Profile card */}
+
+          {/* Tech stack list div */}
+          {id === 3 && (
+            <div className="flex gap-2 lg:gap-6 w-fit absolute -right-3 lg:-right-0">
+              {/* tech stack lists */}
+              <div className="flex flex-col gap-2 md:gap-3 lg:gap-8">
+                {leftLists.map((item, i) => (
+                  <span
+                    key={i}
+                    className={`lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E] text-gray-100`}
+                  >
+                    <item.icon fill={`${item.color}`} className=" w-8 h-8" />
+                  </span>
+                ))}
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+              </div>
+              <div className="flex flex-col gap-2 md:gap-3 lg:gap-8">
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+                {middleLists.map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    <item.icon color={`${item.color}`} className=" w-8 h-8" />
+                  </span>
+                ))}
+              </div>
+              <div className="flex flex-col gap-2 md:gap-3 lg:gap-8">
+                {rightLists.map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    <item.icon color={`${item.color}`} className=" w-8 h-8" />
+                  </span>
+                ))}
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+              </div>
+            </div>
+          )}
+
+          {id === 6 && (
+            <div className="mt-5 relative">
+              {/* button border magic from tailwind css buttons  */}
+              {/* add rounded-md h-8 md:h-8, remove rounded-full */}
+              {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
+              {/* add handleCopy() for the copy the text */}
+              <div
+                className={`absolute -bottom-5 right-10 ${
+                  copied ? "block" : "block"
+                }`}
+              >
+                {copied && <img src="/confetti.gif" alt="confetti" />}
+              </div>
+
+              <MyButton
+                title={
+                  copied ? "Whaou, email copié!" : "Copie mon adresse email"
+                }
+                icon={<IoCopyOutline />}
+                handleClick={handleCopy}
+                position="left"
+                otherClasses="bg-[#161A31]"
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
