af71b6fec5877019a5465cd5ec3406e05bc84ff8
Sma-source <rabhi.smain@gmail.com>
2024-07-10 17:31:32 +0200

    import components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
 import Header from "@/components/Header";
 import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNavbar } from "@/components/ui/FloatingNavbar";
+import { navItems } from "@/data";
+import Testimonials from "@/components/Testimonials";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNavbar navItems={navItems} />
         <Header />
         <Grid />
+        <RecentProjects />
+        <Testimonials />
+        <Experience />
+        <Footer />
       </div>
     </main>
   );
