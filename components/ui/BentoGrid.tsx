7b1ef3cf9bd4dd88b2a73631fbd214244344d9b1
Sma-source <rabhi.smain@gmail.com>
2024-07-26 14:05:13 +0200

    undo change: add gradient background

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -123,7 +123,7 @@ export const BentoGridItem = ({
         {id === 6 && (
           // add background animation , remove the p tag
           <div className="flex justify-center h-full">
-            <div className="w-full h-full  overflow-hidden top-0 left-0 ">
+            <div className="w-full h-full absolute overflow-hidden top-0 left-0 gradient-background">
               <div className="absolute  z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
             </div>
           </div>
